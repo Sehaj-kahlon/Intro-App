64baee4d9916e1fb3c2abfc852270286dde81673
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-03-12 12:29:24 +0530

    background and font changes

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,8 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 223, 94, 145),
+          backgroundColor: Color(0XFF2884B8),
           flexibleSpace: ClipRect(
-            // padding: const EdgeInsets.only(top: 20, left: 32, right: 32),
-            // width: double.infinity,
             child: BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 60.0, sigmaY: 60.0),
               child: Container(color: Colors.transparent),
@@ -39,7 +37,7 @@ class MyApp extends StatelessWidget {
             "LinkTree",
             style: TextStyle(
                 letterSpacing: 0.2,
-                fontFamily: 'Roboto-Black',
+                fontFamily: 'Roboto-BoldItalic',
                 color: Colors.black,
                 fontSize: 46,
                 fontWeight: FontWeight.w400),
@@ -50,17 +48,15 @@ class MyApp extends StatelessWidget {
             decoration: BoxDecoration(
                 gradient: LinearGradient(
               colors: [
-                Color(0xFFB880E0),
-                Color(0xFFF385A4),
-                Color.fromARGB(247, 223, 128, 109),
+                Color(0xFFB4CBF0),
+                Color(0xFF718EDD),
               ],
               stops: [
                 0.3,
-                0.6,
                 1,
               ],
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             )),
             child: Padding(
               padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
@@ -74,108 +70,22 @@ class MyApp extends StatelessWidget {
                         fontSize: 32,
                         fontWeight: FontWeight.w500,
                         letterSpacing: 0.3)),
-                // SizedBox(height: 5),
                 Text(
                     "Bachelor of Electrical Engineering Student at Punjab Engineering College, Chandigarh",
                     style: TextStyle(
                       fontSize: 16,
                     )),
                 SizedBox(height: 15),
-                Scrollbar(
-                  child: options(),
+                RawScrollbar(
+                  thumbColor: Color(0XFF2884B8),
+                  radius: Radius.circular(20),
                   thickness: 4,
-                )
+                  child: options(),
+                ),
               ]),
             ),
           ),
         ),
-        //   child: Column(
-        //     mainAxisAlignment: MainAxisAlignment.center,
-        //     children: <Widget>[
-        //       CircleAvatar(
-        //         radius: 50.0,
-        //         backgroundColor: Colors.green,
-        //         backgroundImage: AssetImage('images/dummy2.JPG'),
-        //       ),
-        //       Text(
-        //         'Sehaj Kahlon',
-        //         style: TextStyle(
-        //             fontFamily: 'Roboto-Italic',
-        //             fontSize: 60.0,
-        //             fontWeight: FontWeight.bold,
-        //             letterSpacing: 2.0,
-        //             color: Colors.white),
-        //       ),
-        //       Text(
-        //         'Student',
-        //         style: TextStyle(
-        //           fontSize: 20.0,
-        //           color: Colors.white,
-        //           fontWeight: FontWeight.bold,
-        //           letterSpacing: 2.0,
-        //         ),
-        //       ),
-        //       Card(
-        //         color: Colors.white,
-        //         margin: EdgeInsets.symmetric(
-        //           horizontal: 25.0,
-        //           vertical: 10.0,
-        //         ),
-        //         child: ListTile(
-        //           leading: Icon(
-        //             Icons.phone,
-        //             color: Colors.lightBlueAccent,
-        //           ),
-        //           title: Text(
-        //             '+91 XXXXXXXXXX',
-        //             style: TextStyle(
-        //               color: Colors.lightBlueAccent,
-        //               fontWeight: FontWeight.bold,
-        //               letterSpacing: 1.0,
-        //               fontSize: 20.0,
-        //             ),
-        //           ),
-        //         ),
-        //       ),
-        //       Card(
-        //         color: Colors.white,
-        //         margin: EdgeInsets.symmetric(
-        //           horizontal: 25.0,
-        //           vertical: 10.0,
-        //         ),
-        //         child: ListTile(
-        //           leading: Icon(Icons.email, color: Colors.lightBlueAccent),
-        //           title: Text('Sehajkahlon437@gmail.com',
-        //               style: TextStyle(
-        //                 color: Colors.lightBlueAccent,
-        //                 fontWeight: FontWeight.bold,
-        //                 letterSpacing: 1.0,
-        //                 fontSize: 20.0,
-        //               )),
-        //         ),
-        //       ),
-        //       Card(
-        //         color: Colors.white,
-        //         margin: EdgeInsets.symmetric(
-        //           horizontal: 25.0,
-        //           vertical: 10.0,
-        //         ),
-        //         child: ListTile(
-        //           leading: Icon(Icons.message, color: Colors.lightBlueAccent),
-        //           // subtitle: Text('Check for subtitle'),
-        //           title: Text('Linkedin',
-        //               style: TextStyle(
-        //                 color: Colors.lightBlueAccent,
-        //                 fontWeight: FontWeight.bold,
-        //                 letterSpacing: 1.0,
-        //                 fontSize: 20.0,
-        //               )),
-        //           onTap: () {},
-        //         ),
-        //       ),
-        //     ],
-        //   ),
-        // ),
       ),
     );
   }
